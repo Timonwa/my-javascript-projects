2f3fba11e6157e3b17d3bbc4b4c60c42d79c69fc
Pelumi Eyitimonwa Akintokun <pelumiakintokun@gmail.com>
2021-07-10 14:23:45 +0100

    added modal project.

---
+++ 5-modal/index.js
@@ -0,0 +1,15 @@
+// varibles
+const openBTn = document.querySelector(".open");
+const closeBtn = document.querySelector(".close");
+const popUp = document.querySelector(".pop-up");
+
+// open function
+openBTn.addEventListener("click", function()
+{
+    popUp.classList.add("modal-toggle");
+});
+// close function
+closeBtn.addEventListener("click", function()
+{
+    popUp.classList.remove("modal-toggle");
+});
